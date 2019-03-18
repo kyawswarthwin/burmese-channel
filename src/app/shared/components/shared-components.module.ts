327446f35f83647408e86d484d977098f3179081
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-19 04:19:36 +0630

    bugfix

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { MessageViewComponent } from './message-view/message-view.component';
+import { SortComponent } from './sort/sort.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule],
-  declarations: [MessageViewComponent],
-  exports: [MessageViewComponent]
+  declarations: [MessageViewComponent, SortComponent, VideoComponent],
+  exports: [MessageViewComponent, SortComponent, VideoComponent]
 })
 export class SharedComponentsModule {}
