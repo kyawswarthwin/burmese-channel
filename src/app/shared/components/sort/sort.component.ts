47388aa7a7294934d2306581bc58d0ef0cb80859
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-19 04:29:03 +0630

    bugfix

--- src/app/shared/components/sort/sort.component.ts
+++ src/app/shared/components/sort/sort.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, Input } from '@angular/core';
+
+import { BasePage } from '../../pages/base/base.page';
 
 @Component({
   selector: 'app-sort',
   templateUrl: './sort.component.html',
-  styleUrls: ['./sort.component.scss'],
+  styleUrls: ['./sort.component.scss']
 })
-export class SortComponent implements OnInit {
+export class SortComponent extends BasePage {
+  @Input() fields: any[];
+  @Input() field: string;
+  @Input() direction: string;
 
-  constructor() { }
+  constructor(public injector: Injector) {
+    super(injector);
+  }
 
-  ngOnInit() {}
-
+  close() {
+    this.closePopover({
+      field: this.field,
+      direction: this.direction
+    });
+  }
 }
