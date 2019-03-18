076fcb0aa33b27df5b265998ef1a2c34890bd28e
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-19 04:10:14 +0630

    bugfix

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-import { LoadingViewComponent } from './loading-view/loading-view.component';
 import { MessageViewComponent } from './message-view/message-view.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule],
-  declarations: [LoadingViewComponent, MessageViewComponent],
-  exports: [LoadingViewComponent, MessageViewComponent]
+  declarations: [MessageViewComponent],
+  exports: [MessageViewComponent]
 })
 export class SharedComponentsModule {}
