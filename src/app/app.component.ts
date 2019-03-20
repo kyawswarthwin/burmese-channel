c0b537a82716545ca7f8e6e04a69087dc0a0b3d6
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-21 06:07:19 +0630

    bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppUpdate } from '@ionic-native/app-update/ngx';
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 
@@ -18,7 +17,6 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private router: Router,
     private appUpdate: AppUpdate,
     private insomnia: Insomnia,
@@ -29,7 +27,6 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.backgroundColorByHexString('#d32f2f');
       this.insomnia.keepAwake();
       this.appUpdate.checkAppUpdate(environment.updateUrl);
       this.parse.initialize(environment.parseConfig);
