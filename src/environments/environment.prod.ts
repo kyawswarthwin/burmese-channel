7c665326212adbfd772a423c7a104c5575a3a50d
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-21 06:12:37 +0630

    bugfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,15 +4,5 @@ export const environment = {
     appId: '2d5e6700-231d-4dc1-9baf-54661e6200b3',
     serverURL: 'https://www.burmesechannel.com/parse'
   },
-  updateUrl: 'https://www.burmesechannel.com/update.xml',
-  adUnits: {
-    ios: {
-      banner: 'ca-app-pub-9704013464677272/1202391879',
-      interstitial: 'ca-app-pub-9704013464677272/3450594346'
-    },
-    android: {
-      banner: 'ca-app-pub-9704013464677272/7189215722',
-      interstitial: 'ca-app-pub-9704013464677272/1936889041'
-    }
-  }
+  updateUrl: 'https://www.burmesechannel.com/update.xml'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,5 @@ export const environment = {
     appId: 'myAppId',
     serverURL: 'http://192.168.1.100/parse'
   },
-  updateUrl: 'http://192.168.1.100/update.xml',
-  adUnits: {
-    ios: {
-      banner: 'ca-app-pub-3940256099942544/6300978111',
-      interstitial: 'ca-app-pub-3940256099942544/1033173712'
-    },
-    android: {
-      banner: 'ca-app-pub-3940256099942544/6300978111',
-      interstitial: 'ca-app-pub-3940256099942544/1033173712'
-    }
-  }
+  updateUrl: 'http://192.168.1.100/update.xml'
 };
