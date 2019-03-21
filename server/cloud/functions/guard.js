37dca7ac3f78cc0e9ebfef21a8c1ac48a3b7a21d
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-21 08:32:59 +0630

    bugfix

--- server/cloud/functions/guard.js
+++ server/cloud/functions/guard.js
@@ -3,7 +3,7 @@
 function isCountrySupported(request) {
   return new Promise(async (resolve, reject) => {
     try {
-      const supportedCountries = (await Parse.Config.get()).get('supportedCountries');
+      const supportedCountries = (await Parse.Config.get()).get('supportedCountries') || [];
       const country = request.headers['cf-ipcountry'];
       const countrySupported = supportedCountries.includes(country);
       resolve(countrySupported);
