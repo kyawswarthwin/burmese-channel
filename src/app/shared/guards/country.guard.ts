45139c6ae5958edbb2a30b06c288ef77fcfda81c
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-21 08:15:39 +0630

    bugfix

--- src/app/shared/guards/country.guard.ts
+++ src/app/shared/guards/country.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import * as Parse from 'parse';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,10 +11,14 @@ export class CountryGuard implements CanActivate {
   constructor(private router: Router) {}
 
   async canActivate(): Promise<boolean> {
-    const countrySupported = await Parse.Cloud.run('isCountrySupported');
-    if (!countrySupported) {
-      this.router.navigate(['/forbidden']);
+    if (environment.production) {
+      const countrySupported = await Parse.Cloud.run('isCountrySupported');
+      if (!countrySupported) {
+        this.router.navigate(['/forbidden']);
+      }
+      return countrySupported;
+    } else {
+      return true;
     }
-    return countrySupported;
   }
 }
