353ba440c1d3e3a84a906f5f0ea39c5f50f8c7ba
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-24 04:46:08 +0630

    bugfix

--- server/app.js
+++ server/app.js
@@ -102,11 +102,8 @@ app.use('/dashboard', dashboard);
 
 app.get('/channels/mrtv.m3u8', async (req, res) => {
   try {
-    const response = await Parse.Cloud.httpRequest({
-      url:
-        'http://api.new.livestream.com/accounts/15604755/events/4419934/broadcasts/189035634.m3u8'
-    });
-    res.redirect(response.headers.location);
+    const url = await getM3u8Url('15604755', '4419934');
+    res.redirect(url);
   } catch (error) {
     res.status(500).json({
       error: 'Internal Server Error'
@@ -116,10 +113,8 @@ app.get('/channels/mrtv.m3u8', async (req, res) => {
 
 app.get('/channels/myanmar_international.m3u8', async (req, res) => {
   try {
-    const response = await Parse.Cloud.httpRequest({
-      url: 'http://api.new.livestream.com/accounts/7063221/events/2739096/broadcasts/189049876.m3u8'
-    });
-    res.redirect(response.headers.location);
+    const url = await getM3u8Url('7063221', '2739096');
+    res.redirect(url);
   } catch (error) {
     res.status(500).json({
       error: 'Internal Server Error'
@@ -132,3 +127,19 @@ server.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
 ParseServer.createLiveQueryServer(server);
+
+function getM3u8Url(account_id, event_id) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = await Parse.Cloud.httpRequest({
+        url: `https://api.new.livestream.com/accounts/${account_id}/events/${event_id}/stream_info`
+      });
+      response = await Parse.Cloud.httpRequest({
+        url: response.data.m3u8_url
+      });
+      resolve(response.headers.location);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
