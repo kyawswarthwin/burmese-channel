e6dec47504eabfccbcbab22a064d8b036966ffad
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-26 19:16:43 +0630

    bugfix

--- server/cloud/jobs/livestream.js
+++ server/cloud/jobs/livestream.js
@@ -28,7 +28,9 @@ async function watchLivestream(request) {
     channel.id = id;
     await channel.save({ name, url });
   });
-  watchLivestream(request);
+  setTimeout(() => {
+    watchLivestream(request);
+  }, 20000);
 }
 
 module.exports = {
