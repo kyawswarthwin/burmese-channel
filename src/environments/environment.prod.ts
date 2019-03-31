87efc585b31c16d85eb31c75d6e0797e986e8883
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-03-31 22:19:23 +0630

    update cordova version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   parseConfig: {
     appId: '2d5e6700-231d-4dc1-9baf-54661e6200b3',
-    serverURL: 'https://www.burmesechannel.com/parse'
+    serverURL: 'http://www.burmesechannel.com/parse'
   },
-  updateUrl: 'https://www.burmesechannel.com/update.xml'
+  updateUrl: 'http://www.burmesechannel.com/update.xml'
 };
